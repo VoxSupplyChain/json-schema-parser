584345981b5d5bb512e592a60b8c66e3256aa887
Todor Todorov <todor@peychev.com>
2015-01-06 18:00:00 +0100

    enable all tests

--- src/test/scala/json/schema/parser/JsonSchemaDecoderFactoryParserTest.scala
+++ src/test/scala/json/schema/parser/JsonSchemaDecoderFactoryParserTest.scala
@@ -25,182 +25,182 @@ trait ScalazMatchers {
 
 }
 
-class JsonSchemaDecoderFactoryParserTest extends FlatSpec with GeneratorDrivenPropertyChecks with Matchers with ScalazMatchers{
+class JsonSchemaDecoderFactoryParserTest extends FlatSpec with GeneratorDrivenPropertyChecks with Matchers with ScalazMatchers {
 
 
   def parse(s: String) = JsonSchemaParser.parse(s).validation
-//
-//  JsonSchemaParser.getClass.toString should "parse empty schemas" in {
-//
-//    parse(
-//      """
-//        |{}
-//      """.stripMargin) shouldBe a[Success[_, _]]
-//
-//  }
-//
-//  it should "parse schemas with optional valid fields" in {
-//
-//    parse(
-//      """
-//        |{
-//        |"title":"root"
-//        |}
-//      """.stripMargin).map { d: SchemaDocument[Double] => d.title} shouldBe Success(Some("root"))
-//
-//    parse(
-//      """
-//        |{
-//        |"title":10
-//        |}
-//      """.stripMargin).map { d: SchemaDocument[Double] => d.title} shouldBe Failure("String: [--\\(title)]")
-//
-//  }
-//
-//  it should "parse nested schemas" in {
-//
-//    val result = parse(
-//      """
-//        |{
-//        |    "title": "root",
-//        |    "otherSchema": {
-//        |        "title": "nested",
-//        |        "anotherSchema": {
-//        |            "title": "alsoNested"
-//        |        }
-//        |    }
-//        |}
-//      """.stripMargin)
-//
-//    result.map(_.title) shouldBe Success(Some("root"))
-//    result.map(_.nestedSchemas("otherSchema").title) shouldBe Success(Some("nested"))
-//    result.map(_.nestedSchemas("otherSchema").nestedSchemas("anotherSchema").title) shouldBe Success(Some("alsoNested"))
-//
-//
-//  }
-//
-//
-//
-//  it should "parse and validate $schemas" in {
-//
-//    parse(
-//      """
-//        |{
-//        |    "$schema":"http://json-schema.org/schema#"
-//        |}
-//      """.stripMargin).map(_.schema) shouldBe Success(Some(new URI("http://json-schema.org/schema#")))
-//
-//    parse(
-//      """
-//        |{
-//        |    "$schema":"http://json-schema.org/draft-04/schema#"
-//        |}
-//      """.stripMargin).map(_.schema) shouldBe Success(Some(new URI("http://json-schema.org/draft-04/schema#")))
-//
-//    parse(
-//      """
-//        |{
-//        |    "$schema":"http://json-schema.org/draft-03/schema#"
-//        |}
-//      """.stripMargin).map(_.schema) should containFailure("not supported schema")
-//
-//  }
-//
-//
-//  it should "validate id" in {
-//
-//    parse(
-//      """
-//        |{
-//        |    "id":"http://x.y.z/rootschema.json#"
-//        |}
-//      """.stripMargin).map(_.id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
-//
-//    parse(
-//      """
-//        |{
-//        |    "id":"#nested"
-//        |}
-//      """.stripMargin).map(_.id) shouldBe Success(new URI("#nested"))
-//
-//    parse(
-//      """
-//        |{
-//        |    "id":""
-//        |}
-//      """.stripMargin).map(_.schema) should containFailure("not valid id")
-//    parse(
-//      """
-//        |{
-//        |    "id":"#"
-//        |}
-//      """.stripMargin).map(_.schema) should containFailure("not valid id")
-//  }
-//
-//  it should "resolve id based on parent schema" in {
-//
-//    val r = parse(
-//      """
-//        |{
-//        |    "id": "http://x.y.z/rootschema.json#",
-//        |    "schema1": {
-//        |        "id": "#foo"
-//        |    },
-//        |    "schema2": {
-//        |        "id": "otherschema.json",
-//        |        "nested": {
-//        |            "id": "#bar"
-//        |        },
-//        |        "alsonested": {
-//        |            "id": "t/inner.json#a"
-//        |        }
-//        |    },
-//        |    "schema3": {
-//        |        "id": "some://where.else/completely#"
-//        |    }
-//        |}
-//      """.stripMargin)
-//
-//    r.map(_.id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
-//    r.map(_.nestedSchemas("schema1").id) shouldBe Success(new URI("http://x.y.z/rootschema.json#foo"))
-//    r.map(_.nestedSchemas("schema2").id) shouldBe Success(new URI("http://x.y.z/otherschema.json#"))
-//    r.map(_.nestedSchemas("schema2").nestedSchemas("nested").id) shouldBe Success(new URI("http://x.y.z/otherschema.json#bar"))
-//    r.map(_.nestedSchemas("schema2").nestedSchemas("alsonested").id) shouldBe Success(new URI("http://x.y.z/t/inner.json#a"))
-//    r.map(_.nestedSchemas("schema3").id) shouldBe Success(new URI("some://where.else/completely#"))
-//  }
-//
-//  it should "resolve to base schema uri if no id in scope" in {
-//
-//    val r = parse(
-//      """
-//        |{
-//        |    "id": "http://x.y.z/rootschema.json#",
-//        |    "schema4": {
-//        |
-//        |    }
-//        |}
-//      """.stripMargin)
-//
-//    r.map(_.id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
-//    r.map(_.nestedSchemas("schema4").id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
-//  }
-//
-//
-//  it should "resolve agaist empty location if no id" in {
-//
-//    val r = parse(
-//      """
-//        |{
-//        |    "$schema":"http://json-schema.org/draft-04/schema#",
-//        |    "schema4": {
-//        |            "id": "#bar"
-//        |    }
-//        |}
-//      """.stripMargin)
-//
-//    r.map(_.id) shouldBe Success(new URI("#"))
-//    r.map(_.nestedSchemas("schema4").id) shouldBe Success(new URI("#bar"))
-//  }
+
+  JsonSchemaParser.getClass.toString should "parse empty schemas" in {
+
+    parse(
+      """
+        |{}
+      """.stripMargin) shouldBe a[Success[_, _]]
+
+  }
+
+  it should "parse schemas with optional valid fields" in {
+
+    parse(
+      """
+        |{
+        |"title":"root"
+        |}
+      """.stripMargin).map { d: SchemaDocument[Double] => d.title} shouldBe Success(Some("root"))
+
+    parse(
+      """
+        |{
+        |"title":10
+        |}
+      """.stripMargin).map { d: SchemaDocument[Double] => d.title} shouldBe Failure("String: [--\\(title)]")
+
+  }
+
+  it should "parse nested schemas" in {
+
+    val result = parse(
+      """
+        |{
+        |    "title": "root",
+        |    "otherSchema": {
+        |        "title": "nested",
+        |        "anotherSchema": {
+        |            "title": "alsoNested"
+        |        }
+        |    }
+        |}
+      """.stripMargin)
+
+    result.map(_.title) shouldBe Success(Some("root"))
+    result.map(_.nestedSchemas("otherSchema").title) shouldBe Success(Some("nested"))
+    result.map(_.nestedSchemas("otherSchema").nestedSchemas("anotherSchema").title) shouldBe Success(Some("alsoNested"))
+
+
+  }
+
+
+
+  it should "parse and validate $schemas" in {
+
+    parse(
+      """
+        |{
+        |    "$schema":"http://json-schema.org/schema#"
+        |}
+      """.stripMargin).map(_.schema) shouldBe Success(Some(new URI("http://json-schema.org/schema#")))
+
+    parse(
+      """
+        |{
+        |    "$schema":"http://json-schema.org/draft-04/schema#"
+        |}
+      """.stripMargin).map(_.schema) shouldBe Success(Some(new URI("http://json-schema.org/draft-04/schema#")))
+
+    parse(
+      """
+        |{
+        |    "$schema":"http://json-schema.org/draft-03/schema#"
+        |}
+      """.stripMargin).map(_.schema) should containFailure("not supported schema")
+
+  }
+
+
+  it should "validate id" in {
+
+    parse(
+      """
+        |{
+        |    "id":"http://x.y.z/rootschema.json#"
+        |}
+      """.stripMargin).map(_.id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
+
+    parse(
+      """
+        |{
+        |    "id":"#nested"
+        |}
+      """.stripMargin).map(_.id) shouldBe Success(new URI("#nested"))
+
+    parse(
+      """
+        |{
+        |    "id":""
+        |}
+      """.stripMargin).map(_.schema) should containFailure("not valid id")
+    parse(
+      """
+        |{
+        |    "id":"#"
+        |}
+      """.stripMargin).map(_.schema) should containFailure("not valid id")
+  }
+
+  it should "resolve id based on parent schema" in {
+
+    val r = parse(
+      """
+        |{
+        |    "id": "http://x.y.z/rootschema.json#",
+        |    "schema1": {
+        |        "id": "#foo"
+        |    },
+        |    "schema2": {
+        |        "id": "otherschema.json",
+        |        "nested": {
+        |            "id": "#bar"
+        |        },
+        |        "alsonested": {
+        |            "id": "t/inner.json#a"
+        |        }
+        |    },
+        |    "schema3": {
+        |        "id": "some://where.else/completely#"
+        |    }
+        |}
+      """.stripMargin)
+
+    r.map(_.id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
+    r.map(_.nestedSchemas("schema1").id) shouldBe Success(new URI("http://x.y.z/rootschema.json#foo"))
+    r.map(_.nestedSchemas("schema2").id) shouldBe Success(new URI("http://x.y.z/otherschema.json#"))
+    r.map(_.nestedSchemas("schema2").nestedSchemas("nested").id) shouldBe Success(new URI("http://x.y.z/otherschema.json#bar"))
+    r.map(_.nestedSchemas("schema2").nestedSchemas("alsonested").id) shouldBe Success(new URI("http://x.y.z/t/inner.json#a"))
+    r.map(_.nestedSchemas("schema3").id) shouldBe Success(new URI("some://where.else/completely#"))
+  }
+
+  it should "resolve to base schema uri if no id in scope" in {
+
+    val r = parse(
+      """
+        |{
+        |    "id": "http://x.y.z/rootschema.json#",
+        |    "schema4": {
+        |
+        |    }
+        |}
+      """.stripMargin)
+
+    r.map(_.id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
+    r.map(_.nestedSchemas("schema4").id) shouldBe Success(new URI("http://x.y.z/rootschema.json#"))
+  }
+
+
+  it should "resolve agaist empty location if no id" in {
+
+    val r = parse(
+      """
+        |{
+        |    "$schema":"http://json-schema.org/draft-04/schema#",
+        |    "schema4": {
+        |            "id": "#bar"
+        |    }
+        |}
+      """.stripMargin)
+
+    r.map(_.id) shouldBe Success(new URI("#"))
+    r.map(_.nestedSchemas("schema4").id) shouldBe Success(new URI("#bar"))
+  }
 
   it should "decodes schema references" in {
 
@@ -219,14 +219,14 @@ class JsonSchemaDecoderFactoryParserTest extends FlatSpec with GeneratorDrivenPr
         |        }
         |    }
         |}
-        |""".stripMargin)
+        | """.stripMargin)
 
     r.map(_.id) shouldBe Success(new URI("http://my.site/myschema#"))
     r.map(_.definitions("schema1").id) shouldBe Success(new URI("http://my.site/schema1#"))
     r.map(_.definitions("schema2").items.value.head.id) shouldBe Success(new URI("http://my.site/schema1#"))
   }
 
-  implicit val invalidSchemasURI: Gen[URI] = Gen.oneOf(List(new URI("http://swagger.io/v2/schema.json")))
+  implicit val remoteCyclicSchemas: Gen[URI] = Gen.oneOf(List(new URI("http://swagger.io/v2/schema.json")))
 
   implicit val validSchemas: Gen[File] = Gen.oneOf(new File("src/test/resources/json/schema/parser/valid").listFiles(new FilenameFilter {
     override def accept(dir: File, name: String): Boolean = name endsWith ".json"
@@ -236,25 +236,28 @@ class JsonSchemaDecoderFactoryParserTest extends FlatSpec with GeneratorDrivenPr
     override def accept(dir: File, name: String): Boolean = name endsWith ".json"
   }).toList)
 
-//  it should "parse all valid schemas" in {
-//    forAll(validSchemas) {
-//      f: File =>
-//        JsonSchemaParser.parse(f).validation shouldBe a[Success[_, _]]
-//    }
-//  }
-//
-//  it should "fail to parse remote schemas with cyclic reference" in {
-//    forAll(invalidSchemasURI) {
-//      f: URI =>
-//        JsonSchemaParser.parse(f).validation should containFailure("cyclic reference")
-//    }
-//  }
-//
-//  it should "fail to parse schemas with cyclic reference" in {
-//    forAll(cyclicSchemas) {
-//      f: File =>
-//        JsonSchemaParser.parse(f).validation should containFailure("cyclic reference")
-//    }
-//  }
+  it should "parse all valid schemas" in {
+    forAll(validSchemas) {
+      f: File =>
+        JsonSchemaParser.parse(f).validation shouldBe a[Success[_, _]]
+    }
+  }
+
+
+  it should "fail to parse schemas with cyclic reference" in {
+    forAll(cyclicSchemas) {
+      f: File =>
+        JsonSchemaParser.parse(f).validation should containFailure("cyclic reference")
+    }
+  }
+
+
+  it should "fail to parse remote schemas with cyclic reference" in {
+    forAll(remoteCyclicSchemas) {
+      f: URI =>
+        JsonSchemaParser.parse(f).validation should containFailure("cyclic reference")
+    }
+  }
+
 
 }
